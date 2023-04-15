f7dc4da7b4c3884b681e282dafb7cf996adf51e6
muhammadyahyo <muhammadyahyodev@gmail.com>
2023-04-16 01:22:26 +0500

    edit swagger title

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const swaggerOptions = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "Swagger documentation for an ExpressJS application",
+      title: "Nodejs, Expressjs, MongoDB, Mongoose, Swagger",
       version: "1.0.0",
       description: "API documentation for an ExpressJS application",
     },
