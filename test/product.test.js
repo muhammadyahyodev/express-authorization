0013ae7796224a3881ea59b21d4fb33c0ef6ed91
muhammadyahyo <muhammadyahyodev@gmail.com>
2023-04-20 14:18:39 +0500

    updated

--- test/product.test.js
+++ test/product.test.js
@@ -7,7 +7,7 @@ const Product = require("../models/Product.model");
 
 chai.use(chaiHttp);
 
-describe("Testing products service", () => {
+describe("Testing products", () => {
   before(async () => {
     await mongoose.connect("mongodb://localhost:27017/test", {
       useNewUrlParser: true,
@@ -21,42 +21,30 @@ describe("Testing products service", () => {
   });
 
   describe("POST /products", () => {
-    it("should create a new product", (done) => {
+    it("should create a new product", async () => {
       const product = {
         title: "Test Product",
         price: "10",
         description: "This is a test product",
       };
 
-      chai
-        .request(app)
-        .post("/api/product")
-        .send(product)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a("object");
-          res.body.should.have.property("_id");
-          res.body.should.have.property("title").eql("Test Product");
-          res.body.should.have.property("price").eql("10");
-          res.body.should.have
-            .property("description")
-            .eql("This is a test product");
-          done();
-        });
+      const res = await chai.request(app).post("/api/product").send(product);
+
+      expect(res.status).to.equal(200);
+      expect(typeof res.body).to.equal("object");
+      expect(res.body).to.have.property("_id");
+      expect(res.body.title).to.equal("Test Product");
+      expect(res.body.price).to.equal("10");
+      expect(res.body.description).to.equal("This is a test product");
     });
   });
 
   describe("GET /products", () => {
-    it("should get all products", (done) => {
-      chai
-        .request(app)
-        .get("/api/product")
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a("array");
-          // res.body.length.should.be.eql(1);
-          done();
-        });
+    it("should get all products", async () => {
+      const res = await chai.request(app).get("/api/product");
+
+      expect(res.status).to.equal(200);
+      expect(res.body.length).to.equal(1);
     });
   });
 
@@ -67,6 +55,7 @@ describe("Testing products service", () => {
         price: "10",
         description: "This is a test product",
       });
+
       await product.save();
 
       const res = await chai.request(app).get(`/api/product/${product._id}`);
@@ -77,35 +66,23 @@ describe("Testing products service", () => {
       expect(res.body.title).to.equal("Test Product");
       expect(res.body.price).to.equal("10");
       expect(res.body.description).to.equal("This is a test product");
-    }); // Increase the timeout to 15 seconds
-
-    it("should return an error message for invalid id", (done) => {
-      chai
-        .request(app)
-        .get("/api/product/invalid_id")
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.body.should.be.a("object");
-          res.body.should.have
-            .property("message")
-            .eql("Canceled, Invalid ID sent");
-          done();
-        });
     });
 
-    it("should return a message for non-existent product id", (done) => {
+    it("should return an error message for invalid id", async () => {
+      const res = await chai.request(app).get("/api/product/invalid_id");
+
+      expect(res.status).to.equal(400);
+      expect(typeof res.body).to.equal("object");
+      expect(res.body.message).to.equal("Canceled, Invalid ID sent");
+    });
+
+    it("should return a message for non-existent product id", async () => {
       const nonExistentId = new mongoose.Types.ObjectId();
-      chai
-        .request(app)
-        .get("/api/product/" + nonExistentId)
-        .end((err, res) => {
-          res.should.have.status(404);
-          res.body.should.be.a("object");
-          res.body.should.have
-            .property("message")
-            .eql("Product does not exist");
-          done();
-        });
+      const res = await chai.request(app).get("/api/product/" + nonExistentId);
+
+      expect(res.status).to.equal(404);
+      expect(typeof res.body).to.equal("object");
+      expect(res.body.message).to.equal("Product does not exist");
     });
   });
 
@@ -161,62 +138,47 @@ describe("Testing products service", () => {
     let product;
 
     before(async () => {
-      // Create a product to use in the tests
       product = new Product({
         title: "Test Product",
         price: "9.99",
         description: "A product for testing purposes",
       });
+
       await product.save();
     });
 
-    it("should delete a product by ID", (done) => {
-      chai
-        .request(app)
-        .delete(`/api/product/${product.id}`)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.have.property("message").eql("Deleted");
-          res.body.should.have.property("product");
-          res.body.product.should.have.property("title").eql("Test Product");
-          res.body.product.should.have.property("price").eql("9.99");
-          res.body.product.should.have
-            .property("description")
-            .eql("A product for testing purposes");
-          done();
-        });
-    });
-
-    it("should return a 400 status if an invalid ID is sent", (done) => {
-      chai
-        .request(app)
-        .delete("/api/product/invalid-id")
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.body.should.have
-            .property("message")
-            .eql("Canceled, Invalid ID sent");
-          done();
-        });
-    });
-
-    it("should return a 400 status if the product was not found", (done) => {
-      chai
-        .request(app)
-        .delete(`/api/product/${product.id}`)
-        .end(async (err, res) => {
-          res.should.have.status(400);
-          res.body.should.have.property("message").eql("Product was not found");
-          // Check that the product was actually deleted from the database
-          const deletedProduct = await Product.findById(product.id);
-          chai.expect(deletedProduct).to.be.null;
-          done();
-        });
-    });
-
     after(async () => {
-      // Clean up the test database by removing the product
       await Product.deleteMany({});
+    });
+
+    it("should delete a product by ID", async () => {
+      const res = await chai.request(app).delete(`/api/product/${product.id}`);
+
+      expect(res.status).to.equal(200);
+      expect(res.body.message).to.equal("Deleted");
+      expect(res.body).to.have.property("product");
+      expect(res.body.product.title).to.equal("Test Product");
+      expect(res.body.product.price).to.equal("9.99");
+      expect(res.body.product.description).to.equal(
+        "A product for testing purposes"
+      );
+    });
+
+    it("should return a 400 status if an invalid ID is sent", async () => {
+      const res = await chai.request(app).delete("/api/product/invalid-id");
+
+      expect(res.status).to.equal(400);
+      expect(res.body.message).to.equal("Canceled, Invalid ID sent");
+    });
+
+    it("should return a 400 status if the product was not found", async () => {
+      const res = await chai.request(app).delete(`/api/product/${product.id}`);
+
+      expect(res.status).to.equal(400);
+      expect(res.body.message).to.equal("Product was not found");
+
+      const deletedProduct = await Product.findById(product.id);
+      chai.expect(deletedProduct).to.be.null;
     });
   });
 });
