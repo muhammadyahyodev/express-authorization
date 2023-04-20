322aa3577a839cc4c87795a73fa94aa266f28ea2
muhammadyahyo <muhammadyahyodev@gmail.com>
2023-04-20 17:01:19 +0500

    add auth bearer to services

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,6 +2,71 @@ const Product = require("../models/Product.model");
 const ApiError = require("../errors/ApiError");
 const mongoose = require("mongoose");
 
+/**
+ * @swagger
+ * /product:
+ *   post:
+ *     summary: Create a new product
+ *     description: Creates a new product with the specified details. Requires a JSON object with title, price, and description fields in the request body.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - title
+ *               - price
+ *               - description
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 description: The title of the product
+ *               price:
+ *                 type: string
+ *                 description: The price of the product
+ *               description:
+ *                 type: string
+ *                 description: A description of the product
+ *     responses:
+ *       200:
+ *         description: Product successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 _id:
+ *                   type: string
+ *                   description: The unique ID of the product
+ *                 title:
+ *                   type: string
+ *                   description: The title of the product
+ *                 price:
+ *                   type: string
+ *                   description: The price of the product
+ *                 description:
+ *                   type: string
+ *                   description: A description of the product
+ *                 createdAt:
+ *                   type: string
+ *                   format: date-time
+ *                   description: The date and time the product was created
+ *                 updatedAt:
+ *                   type: string
+ *                   format: date-time
+ *                   description: The date and time the product was last updated
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: An error message indicating what went wrong
+ */
 const createProduct = async (req, res) => {
   try {
     const { title, price, description } = req.body;
@@ -23,6 +88,53 @@ const createProduct = async (req, res) => {
   }
 };
 
+/**
+ * @swagger
+ * /product:
+ *   get:
+ *     summary: Get a list of products
+ *     description: Returns a list of all products in the database.
+ *     responses:
+ *       200:
+ *         description: List of products retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                     description: The unique ID of the product
+ *                   title:
+ *                     type: string
+ *                     description: The title of the product
+ *                   price:
+ *                     type: string
+ *                     description: The price of the product
+ *                   description:
+ *                     type: string
+ *                     description: A description of the product
+ *                   createdAt:
+ *                     type: string
+ *                     format: date-time
+ *                     description: The date and time the product was created
+ *                   updatedAt:
+ *                     type: string
+ *                     format: date-time
+ *                     description: The date and time the product was last updated
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: An error message indicating what went wrong
+ */
 const getProducts = async (req, res) => {
   try {
     const products = await Product.find({});
@@ -35,6 +147,68 @@ const getProducts = async (req, res) => {
   }
 };
 
+/**
+ * @swagger
+ * /product/{id}:
+ *   get:
+ *     summary: Get a product by ID
+ *     description: Returns the details of a product with the specified ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the product to retrieve.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Product retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 _id:
+ *                   type: string
+ *                   description: The unique ID of the product
+ *                 title:
+ *                   type: string
+ *                   description: The title of the product
+ *                 price:
+ *                   type: string
+ *                   description: The price of the product
+ *                 description:
+ *                   type: string
+ *                   description: A description of the product
+ *                 createdAt:
+ *                   type: string
+ *                   format: date-time
+ *                   description: The date and time the product was created
+ *                 updatedAt:
+ *                   type: string
+ *                   format: date-time
+ *                   description: The date and time the product was last updated
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: An error message indicating what went wrong
+ *       404:
+ *         description: Product not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: An error message indicating what went wrong
+ */
 const getProductById = async (req, res) => {
   try {
     const { id } = req.params;
@@ -58,6 +232,75 @@ const getProductById = async (req, res) => {
   }
 };
 
+/**
+ * @swagger
+ *
+ * /product/{id}:
+ *   put:
+ *     summary: Update a product by ID
+ *     description: Update a product with the specified ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the product to update.
+ *         schema:
+ *           type: string
+ *           format: uuid
+ *           example: 614ebe141649c20016447f4c
+ *       - in: body
+ *         name: body
+ *         required: true
+ *         description: The product object that needs to be updated.
+ *         schema:
+ *           type: object
+ *           properties:
+ *             title:
+ *               type: string
+ *             price:
+ *               type: string
+ *             description:
+ *               type: string
+ *     responses:
+ *       '200':
+ *         description: Successfully updated the product.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Updated
+ *                 product:
+ *                   $ref: '#/components/schemas/Product'
+ *       '400':
+ *         description: Invalid ID sent.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Canceled, Invalid ID sent
+ *       '404':
+ *         description: Product not found.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Product does not exist
+ *       '500':
+ *         description: Internal server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ApiError'
+ */
 const updateProductById = async (req, res) => {
   try {
     const { id } = req.params;
@@ -83,6 +326,63 @@ const updateProductById = async (req, res) => {
   }
 };
 
+/**
+ * @swagger
+ * /product/{id}:
+ *   delete:
+ *     summary: Delete a product by ID
+ *     description: Deletes a product by ID from the database.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         description: ID of the product to delete
+ *         schema:
+ *           type: string
+ *           format: objectId
+ *         required: true
+ *         example: 610c1975b5d5a728184cc49d
+ *     responses:
+ *       200:
+ *         description: Product deleted successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: A message indicating the status of the deletion operation
+ *                   example: Deleted
+ *                 product:
+ *                   $ref: '#/components/schemas/Product'
+ *                   description: The deleted product object
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *                   example: Product was not found
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *                   example: Internal Error
+ *                 friendlyMsg:
+ *                   type: string
+ *                   description: A friendly message for the client
+ *                   example: Sorry, something went wrong on our end.
+ */
 const deleteProductById = async (req, res) => {
   try {
     const { id } = req.params;
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,35 +6,80 @@ const ApiError = require("../errors/ApiError");
 const mongoose = require("mongoose");
 
 /**
+ * Registers a new user.
+ *
  * @swagger
  * /user/signup:
  *   post:
- *     summary: create new user
- *     tags: [User]
+ *     summary: Register a new user
+ *     produces:
+ *       - application/json
  *     parameters:
- *       - full_name: Jasur Hamidov
- *         description: fullname of user
- *         in: formData
- *         required: true
- *         type: string
- *       - email: jasur@gmail.com
- *         description: email of user
- *         in: formData
- *         required: true
- *         type: string
- *       - password: qwerty
- *         description: password of user
- *         in: formData
- *         required: true
- *         type: string
- *       - is_active: false
- *         description: status of user
- *         in: formData
- *         required: false
- *         type: boolean
+ *       - in: body
+ *         name: user
+ *         description: The user to be created
+ *         schema:
+ *           type: object
+ *           required:
+ *             - full_name
+ *             - email
+ *             - password
+ *           properties:
+ *             full_name:
+ *               type: string
+ *               description: The full name of the user
+ *             email:
+ *               type: string
+ *               description: The email address of the user
+ *             password:
+ *               type: string
+ *               description: The password of the user
  *     responses:
  *       200:
- *         description: Success
+ *         description: Successfully registered a new user
+ *         schema:
+ *           type: object
+ *           properties:
+ *             user:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: string
+ *                   description: The ID of the user
+ *                 full_name:
+ *                   type: string
+ *                   description: The full name of the user
+ *                 email:
+ *                   type: string
+ *                   description: The email address of the user
+ *                 password:
+ *                   type: string
+ *                   description: The password of the user
+ *                 is_active:
+ *                   type: boolean
+ *                   description: The activation status of the user
+ *             token:
+ *               type: string
+ *               description: The authentication token of the user
+ *       403:
+ *         description: Gmail cannot be duplicated
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               description: The error message
+ *       500:
+ *         description: Internal error occurred
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               description: The error message
+ *             friendlyMsg:
+ *               type: string
+ *               description: A friendly message to be displayed to the user
  */
 const signUp = async (req, res) => {
   try {
@@ -87,27 +132,88 @@ const signUp = async (req, res) => {
 };
 
 /**
+ * Authenticates an existing user.
+ *
  * @swagger
  * /user/signin:
  *   post:
- *     summary: login user
- *     tags: [User]
+ *     summary: Authenticate an existing user
+ *     produces:
+ *       - application/json
  *     parameters:
- *       - email: jasur@gmail.com
- *         description: email of user
- *         in: formData
- *         required: true
- *         type: string
- *       - password: qwerty
- *         description: password of user
- *         in: formData
- *         required: true
- *         type: string
+ *       - in: body
+ *         name: user
+ *         description: The user to be authenticated
+ *         schema:
+ *           type: object
+ *           required:
+ *             - email
+ *             - password
+ *           properties:
+ *             email:
+ *               type: string
+ *               description: The email address of the user
+ *             password:
+ *               type: string
+ *               description: The password of the user
+ *     security:
+ *        - bearerAuth: []
  *     responses:
  *       200:
- *         description: Success
- *
+ *         description: Successfully authenticated user
+ *         schema:
+ *           type: object
+ *           properties:
+ *             user:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: string
+ *                   description: The ID of the user
+ *                 full_name:
+ *                   type: string
+ *                   description: The full name of the user
+ *                 email:
+ *                   type: string
+ *                   description: The email address of the user
+ *                 password:
+ *                   type: string
+ *                   description: The password of the user
+ *                 is_active:
+ *                   type: boolean
+ *                   description: The activation status of the user
+ *             token:
+ *               type: string
+ *               description: The authentication token of the user
+ *       400:
+ *         description: Password is incorrect
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               description: The error message
+ *       404:
+ *         description: User does not exist
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               description: The error message
+ *       500:
+ *         description: Internal error occurred
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               description: The error message
+ *             friendlyMsg:
+ *               type: string
+ *               description: A friendly message to be displayed to the user
  */
+
 const signIn = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -153,6 +259,38 @@ const signIn = async (req, res) => {
   }
 };
 
+/**
+ * @swagger
+ * /api/users/logout:
+ *   post:
+ *     summary: Logout user
+ *     description: Logout user by removing token and updating user status
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Logout successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 user:
+ *                   $ref: '#/components/schemas/User'
+ *                 message:
+ *                   type: string
+ *                   description: Logout message
+ *       400:
+ *         description: Invalid token or token does not exist
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ */
 const logout = async (req, res) => {
   try {
     const { token } = req.cookies;
@@ -186,12 +324,34 @@ const logout = async (req, res) => {
  * @swagger
  * /user:
  *   get:
- *     summary: get all users
- *     tags: [User]
+ *     summary: Get all users
+ *     description: Returns a list of all users
+ *     produces:
+ *       - application/json
  *     responses:
  *       200:
- *         description: Muvaffaqiyatli so'rov
+ *         description: A list of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *                 friendlyMsg:
+ *                   type: string
+ *                   description: Friendly error message
  */
+
 const getUsers = async (req, res) => {
   try {
     const users = await User.find({});
@@ -207,20 +367,61 @@ const getUsers = async (req, res) => {
 
 /**
  * @swagger
- * /user:id:
+ * /user/{id}:
  *   get:
- *     summary: get user by id
- *     tags: [User]
+ *     summary: Get a user by ID
+ *     description: Returns a single user based on ID
  *     parameters:
- *       - id: 643a8f89b7cadc06295280e1
- *         description: user id
- *         in: formData
+ *       - in: path
+ *         name: id
+ *         description: ID of the user to retrieve
  *         required: true
- *         type: string
+ *         schema:
+ *           type: string
+ *     produces:
+ *       - application/json
  *     responses:
  *       200:
- *         description: Success
+ *         description: A single user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       400:
+ *         description: Invalid ID sent
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *       404:
+ *         description: User does not exist
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *                 friendlyMsg:
+ *                   type: string
+ *                   description: Friendly error message
  */
+
 const getUserById = async (req, res) => {
   try {
     const { id } = req.params;
@@ -246,34 +447,73 @@ const getUserById = async (req, res) => {
 
 /**
  * @swagger
- * /user/:id:
+ * /user/{id}:
  *   put:
- *     summary: Update user
- *     tags: [User]
+ *     summary: Update a user by ID
+ *     description: Update a user based on ID
  *     parameters:
- *       - full_name: Davron Ergashev
- *         description: User name
- *         in: formData
+ *       - in: path
+ *         name: id
+ *         description: ID of the user to update
  *         required: true
- *         type: string
- *       - email: davron@gmail.com
- *         description: email of user
- *         in: formData
+ *         schema:
+ *           type: string
+ *       - in: body
+ *         name: body
+ *         description: Request body to update user information
  *         required: true
- *         type: string
- *       - password: ytreewq
- *         description: password of user
- *         in: formData
- *         required: true
- *         type: string
- *       - is_active: false
- *         description: status user
- *         in: formData
- *         required: false
- *         type: boolean
+ *         schema:
+ *           type: object
+ *           properties:
+ *             full_name:
+ *               type: string
+ *               description: Full name of the user
+ *             email:
+ *               type: string
+ *               description: Email of the user
+ *             password:
+ *               type: string
+ *               description: Password of the user
+ *     security:
+ *       - bearerAuth: []
+ *     produces:
+ *       - application/json
  *     responses:
  *       200:
- *         description: Foydalanuvchi muvaffaqiyatli qo'shildi
+ *         description: User updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Success message
+ *                 user:
+ *                   $ref: '#/components/schemas/User'
+ *       400:
+ *         description: Invalid ID sent or User does not exist
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *                 friendlyMsg:
+ *                   type: string
+ *                   description: Friendly error message
  */
 const updateUserById = async (req, res) => {
   try {
@@ -319,20 +559,63 @@ const updateUserById = async (req, res) => {
 
 /**
  * @swagger
- * /user/:id:
+ * /user/{id}:
  *   delete:
- *     summary: Delete user by id
- *     tags: [User]
+ *     summary: Delete a user by ID
+ *     description: Deletes a user with the specified ID. Requires a valid user ID in the URL parameter.
  *     parameters:
- *       - id: user name
- *         description: full name of user
- *         in: formdata
+ *       - in: path
+ *         name: id
  *         required: true
- *         type: string
+ *         description: ID of the user to delete
+ *         schema:
+ *           type: string
+ *     security:
+ *       - bearerAuth: []
  *     responses:
  *       200:
- *         description: Success
+ *         description: User successfully deleted
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: A success message indicating that the user was deleted
+ *                 user:
+ *                   type: object
+ *                   description: The deleted user object
+ *                   properties:
+ *                     _id:
+ *                       type: string
+ *                       description: The unique ID of the user
+ *                     name:
+ *                       type: string
+ *                       description: The name of the user
+ *                     email:
+ *                       type: string
+ *                       description: The email address of the user
+ *                     createdAt:
+ *                       type: string
+ *                       format: date-time
+ *                       description: The date and time the user was created
+ *                     updatedAt:
+ *                       type: string
+ *                       format: date-time
+ *                       description: The date and time the user was last updated
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: An error message indicating what went wrong
  */
+
 const deleteUserById = async (req, res) => {
   try {
     const { id } = req.params;
